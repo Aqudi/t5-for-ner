f40c45b2e9109051e8bb241c1cb0563291467ef6
Aqudi <gjdigj145@gmail.com>
2022-10-17 16:48:34 +0900

    폴더 구조 변경 & 스크립트 경로 수정

--- preprocess.sh
+++ preprocess.sh
@@ -2,6 +2,12 @@ echo "기존 파일 지우기"
 rm -r ./data_learn/preprocessed
 
 echo "t2t를 위한 데이터 전처리"
-python data/preprocess/preprocess_data_for_t2t.py
+PWD=$(pwd)
+python $PWD/data/preprocess_data_for_t2t.py\
+    --train=$PWD/data_learn/klue_ner_train_80.t\
+    --test=$PWD/data_learn/klue_ner_test_20.t\
+    --dest_train=$PWD/data_learn/preprocessed/klue_ner_t2t_train.tsv\
+    --dest_test=$PWD/data_learn/preprocessed/klue_ner_t2t_test.tsv
+
 
 echo "완료~"
